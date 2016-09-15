b93b18ead96a00e06a730cac656ad2c265cec646
Erika Takahara <erika.takahara@gmail.com>
2016-09-14 22:56:56 -0300

    Adds more urls to cache

--- service.js
+++ service.js
@@ -1,9 +1,7 @@
-importScripts('assets/js/vendor/idb-keyval-min.js');
-
 const CACHE_VERSION = 'v1';
 
 var URLS_TO_CACHE = [
-	'/okiniiri',
+	'/okiniiri/',
 	'assets/js/vendor/async-define.js',
 	'assets/js/vendor/handlebars-v4.0.5.js',
 	'assets/js/vendor/idb-keyval-min.js',
@@ -18,20 +16,26 @@ var URLS_TO_CACHE = [
 	'assets/js/page/connection.js',
 	'assets/css/reset.css',
 	'assets/css/base.css',
-	'https://fonts.googleapis.com/css?family=Delius'
+	'assets/images/logo-ld.png',
+	'assets/images/loading.gif',
+	'assets/images/anonymous.png',
+	'https://fonts.googleapis.com/css?family=Delius',
+	'https://fonts.gstatic.com/s/delius/v6/bDpuiE0ncvY05_AsiZIWR-vvDin1pK8aKteLpeZ5c0A.woff2'
 ];
 
 self.addEventListener('install', function(e) {
 	e.waitUntil(
-		caches.open(CACHE_VERSION).then(function(cache) {
-			return cache.addAll(URLS_TO_CACHE);
-		})
+		caches.open(CACHE_VERSION)
+			.then(function(cache) {
+				return cache.addAll(URLS_TO_CACHE);
+			})
 	);
 });
 
 self.addEventListener('fetch', function(e) {
 	let url = e.request.url;
-	if(url.indexOf('githubusercontent.com') != -1 || url.indexOf('api.github.com/gists') != -1) {
+	if(url.indexOf('githubusercontent.com') != -1
+		|| url.indexOf('api.github.com/gists') != -1) {
 		e.respondWith(fetch(e.request)
 			.then(response => {
 				return caches.open(CACHE_VERSION).then(cache => {
@@ -44,16 +48,7 @@ self.addEventListener('fetch', function(e) {
 			})
 		);
 	} else {
-		e.respondWith(
-			fetch(e.request).then(function(response) {
-				return response;
-			}).catch(function() {
-				return caches.match(e.request)
-					.then(function(response) {
-						return response;
-					})
-			})
-		);
+		e.respondWith(caches.match(e.request));
 	}
 
 });
